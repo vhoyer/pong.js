c0b5d316eb1570eeef1f85a1ad43c18f5b326614
Vinícius Hoyer <vhoyer@live.com>
2020-06-07 21:59:31 -0300

    :construction: work work work

--- src/assets/play-field/index.js
+++ src/assets/play-field/index.js
@@ -1,61 +1,102 @@
-import Collider from '../collider';
-import Ball from './ball';
-import Player from './player';
-import Scoreboard from './scoreboard';
+import mage from '../tiles/mage.png';
+import totem from '../tiles/totem.png';
 
-export default (gameLoop, canvas) => {
-  const scoreboard = new Scoreboard();
-
-  const playerL = new Player({
-    initialX: 0,
-    initialY: canvas.height / 2,
-    side: 'left',
-    upKey: 'w',
-    downKey: 's',
-  });
-
-  const playerR = new Player({
-    initialX: canvas.width,
-    initialY: canvas.height / 2,
-    side: 'right',
-    upKey: 'ArrowUp',
-    downKey: 'ArrowDown',
-  });
-
-  const ball = new Ball({
-    initialX: canvas.width / 2,
-    initialY: canvas.height / 2,
-  });
-
-  const declareWinner = (winner) => {
-    ball.reset();
-    playerL.reset();
-    playerR.reset();
-    scoreboard.incrementScore(winner);
-  };
-
-  const playerLWinningArea = new Collider({
-    x: canvas.width,
-    y: 0,
-    height: canvas.height,
-    width: 100,
-    onCollision: () => declareWinner('left'),
-  });
-
-  const playerRWinningArea = new Collider({
-    x: -100,
-    y: 0,
-    height: canvas.height,
-    width: 100,
-    onCollision: () => declareWinner('right'),
-  });
-
+export default (gameLoop, _canvas) => {
   gameLoop.addObjectsToPipeline(
-    ball,
-    playerL,
-    playerLWinningArea,
-    playerR,
-    playerRWinningArea,
-    scoreboard,
+    {
+      tile: new Image(),
+      vel: 2,
+      vec: { x: 0, y: 0 },
+      pos: { x: 0, y: 0 },
+      tileSize: 64,
+      direction: 0,
+      state: 0,
+      isMoving: false,
+      onSetup() {
+        this.tile.src = mage;
+        document.addEventListener('keydown', ({ key }) => {
+          const events = {
+            /* eslint-disable no-return-assign */
+            h: () => this.vec.x = -this.vel,
+            j: () => this.vec.y = +this.vel,
+            k: () => this.vec.y = -this.vel,
+            l: () => this.vec.x = +this.vel,
+            /* eslint-enable no-return-assign */
+          };
+
+          if (events[key]) events[key]();
+        });
+        document.addEventListener('keyup', ({ key }) => {
+          const events = {
+            /* eslint-disable no-return-assign */
+            h: () => this.vec.x = 0,
+            j: () => this.vec.y = 0,
+            k: () => this.vec.y = 0,
+            l: () => this.vec.x = 0,
+            /* eslint-enable no-return-assign */
+          };
+
+          if (events[key]) events[key]();
+        });
+      },
+      onFixedUpdate() {
+        this.pos.x += this.vec.x;
+        this.pos.y += this.vec.y;
+
+        this.isMoving = this.vec.x !== 0 || this.vec.y !== 0;
+
+        if (this.isMoving) {
+          this.step = (this.step || 0) + 1;
+          // eslint-disable-next-line no-plusplus
+          if (this.step === 30) {
+            this.step = 0;
+            this.state = this.state === 1 ? 2 : 1;
+          }
+
+          this.direction = Number(this.vec.x < 0);
+        } else {
+          this.state = 0;
+          this.step = 0;
+        }
+      },
+      onDraw(game) {
+        const render = game.getRender();
+        render.imageSmoothingEnabled = false;
+
+        render.drawImage(
+          this.tile,
+          16 * this.state,
+          16 * this.direction,
+          16,
+          16,
+          this.pos.x,
+          this.pos.y,
+          this.tileSize,
+          this.tileSize,
+        );
+      },
+    },
+    {
+      tile: new Image(),
+      onSetup() {
+        this.tile.src = totem;
+      },
+      onDraw(game) {
+        const render = game.getRender();
+        render.imageSmoothingEnabled = false;
+
+        render.drawImage(
+          this.tile,
+          0,
+          0,
+          this.tile.width,
+          this.tile.height,
+          29,
+          178,
+          this.tile.width * 6,
+          this.tile.height * 6,
+        );
+      },
+    },
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import GameLoop from './assets/game-loop';
 import playField from './assets/play-field';
+import dirt from './assets/tiles/dirt.png';
 
 const canvas = document.getElementById('game');
 canvas.width = 800 * 1.3;
 canvas.height = 450 * 1.3;
 
+
+const dirtTile = new Image();
+dirtTile.src = dirt;
+const tileSize = 64;
+
 function drawBackground(game) {
   const { width, height } = game;
   const render = game.getRender();
+  render.imageSmoothingEnabled = false;
 
   render.fillStyle = 'black';
   render.fillRect(0, 0, width, height);
+
+  const xScan = Math.ceil(width / tileSize);
+  const yScan = Math.ceil(height / tileSize);
+
+  for (let x = 0; x < xScan; x += 1) {
+    for (let y = 0; y < yScan; y += 1) {
+      render.drawImage(dirtTile, tileSize * x, tileSize * y, tileSize, tileSize);
+    }
+  }
 }
 
 const gameLoop = new GameLoop(canvas);
